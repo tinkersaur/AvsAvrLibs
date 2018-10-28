dab5b54079ea8e1f0611b487bc6179fc61986c4b
avs <avs@myhp>
2018-10-28 10:26:35 -0600

    Minor changes in comments, and some cleaning up.

--- AvrScheduler/010_test_one_thread/010_test_one_thread.ino
+++ AvrScheduler/010_test_one_thread/010_test_one_thread.ino
@@ -1,20 +1,18 @@
 #include"AvrScheduler.h"
 
 void greetings(){
-  unsigned long t = millis();
   Serial.print("Time: ");
-  Serial.print((int)t);
+  Serial.print(millis());
   Serial.println(". Hello!");  
 }
 
 void setup(){
+  // Initialize this first so that tracing can be done.
   Serial.begin(9600);
   init_tasks();
   add_callback_task(1, 250000, greetings);
 }
 
 void loop() {
-  //greetings();
-  delay(100);
   run_task();
 }
