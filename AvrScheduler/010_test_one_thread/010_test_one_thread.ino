f1fe3df06a646f358ce7eeb4096915a07345ea6e
avs <avs@myhp>
2018-11-09 02:34:20 -0700

    010 and 020 are working with AvrScheduler2.

--- AvrScheduler/010_test_one_thread/010_test_one_thread.ino
+++ AvrScheduler/010_test_one_thread/010_test_one_thread.ino
@@ -1,18 +1,33 @@
 #include"AvrScheduler.h"
 
+volatile bool ready;
 void greetings(){
-  Serial.print("Time: ");
-  Serial.print(millis());
-  Serial.println(". Hello!");  
+    ready = true;
 }
+
+int count = 0;
 
 void setup(){
   // Initialize this first so that tracing can be done.
   Serial.begin(9600);
   init_tasks();
-  add_callback_task(1, 250000, greetings);
+  ready = false;
+  add_callback_task(1, 500000, greetings);
+  wake_tasks();
 }
 
 void loop() {
-  run_task();
+  if (ready){
+    Serial.print("Time: ");
+    Serial.print(millis());
+    Serial.println(". Hello!");  
+    ready = false;
+  }
+  // delay(100);
+  // Serial.print("TCNT1: ");
+  // Serial.println(TCNT1);
+  // Serial.print("OCR1A: ");
+  // Serial.println(OCR1A);
+  // Serial.print("wtime: ");
+  // Serial.println(next_task_time());
 }
--- AvrScheduler/020_test_three_threads/020_test_three_threads.ino
+++ AvrScheduler/020_test_three_threads/020_test_three_threads.ino
@@ -1,33 +1,49 @@
 #include"AvrScheduler.h"
 
-void greetingsA(){
-  Serial.print("Time: ");
-  Serial.print(millis());
-  Serial.println(". A");  
-}
+volatile bool cond_a, cond_b, cond_c;
 
+void greetingsA(){
+  cond_a = true;
+}
 
 void greetingsB(){
-  Serial.print("Time: ");
-  Serial.print(millis());
-  Serial.println(". B");  
+  cond_b= true;
 }
-
 
 void greetingsC(){
-  Serial.print("Time: ");
-  Serial.print(millis());
-  Serial.println(". C");  
+  cond_c = true;
 }
+
 void setup(){
   // Initialize this first so that tracing can be done.
-  Serial.begin(9600);
+  Serial.begin(115200);
   init_tasks();
-  add_callback_task(1, 250000, greetingsA);
-  add_callback_task(2, 330000, greetingsB);
-  add_callback_task(3, 500000, greetingsC);
+  add_callback_task(1, 2500000, greetingsA);
+  add_callback_task(2, 3300000, greetingsB);
+  add_callback_task(3, 5000000, greetingsC);
+  cond_a = false;
+  cond_b = false;
+  cond_c = false;
+  wake_tasks();
 }
 
 void loop() {
-  run_task();
+  if (cond_a){ 
+    cond_a = false;
+    Serial.print("Time: ");
+    Serial.print(quick_millis());
+    Serial.println(". A");  
+  }
+  if (cond_b){ 
+    cond_b = false;
+    Serial.print("Time: ");
+    Serial.print(quick_millis());
+    Serial.println(". B");  
+  }
+  if (cond_c){ 
+    cond_c = false;
+    Serial.print("Time: ");
+    Serial.print(quick_millis());
+    Serial.println(". C");  
+  }
 }
